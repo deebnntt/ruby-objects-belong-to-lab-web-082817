62fc155ce4b4cedf10d72eefcc98869ce1ff9542
deebnntt <daniellebennettchan@gmail.com>
2017-08-31 10:26:20 -0400

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,14 @@
+class Artist
+
+attr_accessor :name, :song
+
+  def initialize
+    @name = name
+    @song = song
+  end
+
+  def name
+    @name
+  end
+  
+end
---
+++ lib/author.rb
@@ -0,0 +1,13 @@
+class Author
+
+attr_accessor :name, :artist
+
+  def initialize
+    @name = name
+  end
+
+  def name
+    @name
+  end
+
+end
